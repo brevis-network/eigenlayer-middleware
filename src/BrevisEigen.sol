4a01af3556a065827a5e74a7f694b6ca71e618ba
Elliott He <elliott.he@outlook.com>
2024-07-01 17:38:19 +0800

    batch verify

--- src/BrevisEigen.sol
+++ src/BrevisEigen.sol
@@ -9,10 +9,10 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
     // admin configs
     bytes public quorumNumbers = hex"00_01"; // 2 quorums, value to be determined
 
-    // if request has been verified, will save reqid -> true
+    // if request has been verified, will save reqKey -> true, reqKey = keccak256(abi.encodePacked(_proofId, _nonce));
     mapping(bytes32 => bool) public records;
     // this reqid has been verified by agg bls sig
-    event Verified(bytes32 requestId);
+    event Verified(bytes32 requestId, bytes32[] proofIds, uint64[] nonces);
 
     constructor(
         IAVSDirectory __avsDirectory,
@@ -37,9 +37,12 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
     function verifyRequest(
         bytes32 reqid,
         uint64 blockNum,
+        bytes32[] calldata _proofIds,
+        uint64[] calldata _nonces,
         NonSignerStakesAndSignature memory nonSignerStakesAndSignature
     ) external {
-        bytes32 msghash = keccak256(abi.encodePacked(block.chainid, address(this), "BrevisEigen",reqid, blockNum));
+        require(_proofIds.length == _nonces.length, "length not match");
+        bytes32 msghash = keccak256(abi.encodePacked(block.chainid, address(this), "BrevisEigen", reqid, blockNum));
         bytes memory memQ = quorumNumbers;
         (
             QuorumStakeTotals memory quorumStakeTotals,
@@ -54,14 +57,18 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
             // must over 2/3
             require(quorumStakeTotals.signedStakeForQuorum[i] >= (quorumStakeTotals.totalStakeForQuorum[i]*2)/3+1);
         }
-        records[reqid] = true;
-        emit Verified(reqid);
+        require(reqid == keccak256(abi.encodePacked(_proofIds, _nonces)), "invalid _proofIds and _nonces");
+        for (uint i = 0; i < _proofIds.length; i++) {
+            records[keccak256(abi.encodePacked(_proofIds[i], _nonces[i]))] = true;
+        }
+        emit Verified(reqid, _proofIds, _nonces);
     }
 
     // require all reqIds have been verified
-    function mustVerified(bytes32[] calldata reqIds) public view {
-        for (uint256 i = 0; i < reqIds.length; i++) {
-            require(records[reqIds[i]] == true);
+    function mustVerified(bytes32[] calldata _proofIds, uint64[] calldata _nonces) public view {
+        require(_proofIds.length == _nonces.length, "length not match");
+        for (uint256 i = 0; i < _proofIds.length; i++) {
+            require(records[keccak256(abi.encodePacked(_proofIds[i], _nonces[i]))] == true);
         }
     }
 
