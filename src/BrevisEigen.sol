629fa00bc64c51c8844aac51d41fc7fe2543345c
Xiaozhou Li <xli@celer.network>
2024-07-02 00:13:29 -0700

    simplify verifySigs

--- src/BrevisEigen.sol
+++ src/BrevisEigen.sol
@@ -9,11 +9,6 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
     // admin configs
     bytes public quorumNumbers = hex"00_01"; // 2 quorums, value to be determined
 
-    // if request has been verified, will save reqid -> true
-    mapping(bytes32 => bool) public records;
-    // this reqid has been verified by agg bls sig
-    event Verified(bytes32 requestId);
-
     constructor(
         IAVSDirectory __avsDirectory,
         IRegistryCoordinator __registryCoordinator,
@@ -33,19 +28,18 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
         _transferOwnership(_initialOwner);
     }
 
-    // verify sig
-    function verifyRequest(
-        bytes32 reqid,
+    // verify sigs
+    function verifySigs(
+        bytes32 msgHash,
         uint64 blockNum,
         NonSignerStakesAndSignature memory nonSignerStakesAndSignature
-    ) external {
-        bytes32 msghash = keccak256(abi.encodePacked(block.chainid, address(this), "BrevisEigen",reqid, blockNum));
+    ) external view {
         bytes memory memQ = quorumNumbers;
         (
             QuorumStakeTotals memory quorumStakeTotals,
             bytes32 hashOfNonSigners
         ) = checkSignatures(
-                msghash,
+                msgHash,
                 memQ,
                 uint32(blockNum),
                 nonSignerStakesAndSignature
@@ -53,15 +47,6 @@ contract BrevisEigen is BLSSignatureChecker, ServiceManagerBase {
         for (uint i = 0; i < quorumNumbers.length; i++) {
             // must over 2/3
             require(quorumStakeTotals.signedStakeForQuorum[i] >= (quorumStakeTotals.totalStakeForQuorum[i]*2)/3+1);
-        }
-        records[reqid] = true;
-        emit Verified(reqid);
-    }
-
-    // require all reqIds have been verified
-    function mustVerified(bytes32[] calldata reqIds) public view {
-        for (uint256 i = 0; i < reqIds.length; i++) {
-            require(records[reqIds[i]] == true);
         }
     }
 
